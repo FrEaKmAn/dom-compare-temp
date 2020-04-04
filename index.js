7d38053a4f4225e407c5dc9da04128aebb1f8cf7
Erol Merdanović <zasebnost@gmail.com>
2020-04-04 20:12:33 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  var libPrefix = process.env.COVER ? './lib-cov' : './lib';
+
+  module.exports = {
+    compare: require(libPrefix + '/compare'),
+    XMLSerializer: require(libPrefix + '/canonizer'),
+    revXPath: require(libPrefix + '/revxpath'),
+    GroupingReporter: require(libPrefix + '/reporters/groupingReporter.js')
+  };
+})();
